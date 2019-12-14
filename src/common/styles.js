657d0be7c9aaf31cb5ef4b5de864c583f1b41751
jevvilla <juan.es-19@hotmail.com>
2019-12-14 06:56:23 -0500

    register: some common styles were added

---
+++ src/common/styles.js
@@ -0,0 +1,22 @@
+export const sizes = {
+  // global sizes
+  base: 16,
+  font: 14,
+  radius: 6,
+  padding: 25,
+};
+
+export const colors = {
+  accent: '#8A84E2',
+  background: 'rgb(249, 249, 254)',
+  black: '#323643',
+  dark: '#41463D',
+  deepBlack: '#000',
+  gray2: '#C5CCD6',
+  green: 'rgb(150, 255, 150)',
+  light: '#B8CDF8',
+  red: 'rgb(255,100,100)',
+  secondary: '#95F2D9',
+  transparent: '#00000000',
+  white: '#FFFFFF',
+};
