c7dcd81a6be930c73c91fc4b038b735950df8c4c
jevvilla <juan.es-19@hotmail.com>
2019-12-14 07:03:22 -0500

    register: Login and Register screens UI implemented

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
 
 import * as routes from '../../navigation/routes';
+
+import styles from './styles';
 
 const Login = props => {
   const goToRegister = () => {
@@ -10,6 +13,7 @@ const Login = props => {
     navigation.navigate(routes.REGISTER);
   };
 
+  // eslint-disable-next-line no-unused-vars
   const goToHome = () => {
     const { navigation } = props;
 
@@ -17,16 +21,22 @@ const Login = props => {
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Login</Text>
       <TouchableOpacity onPress={goToRegister}>
         <Text>go to register</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={goToHome}>
+      {/* <TouchableOpacity onPress={goToHome}>
         <Text>go to Home</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   );
+};
+
+Login.propTypes = {
+  navigation: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default Login;
--- src/screens/Register/index.jsx
+++ src/screens/Register/index.jsx
@@ -1,12 +1,71 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+import { Button } from '../../common/components';
+import * as strings from '../../common/strings';
+
+import styles from './styles';
+
+const Register = props => {
+  const [credentials, setCredentials] = useState({});
+
+  const onChangeTextInputHandler = (key, value) => setCredentials({ ...credentials, [key]: value });
+
+  const goBack = () => {
+    const { navigation } = props;
+
+    navigation.goBack();
+  };
+
+  const behavior = Platform.OS === 'android' ? 'height' : 'padding';
+
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior={behavior} enabled>
+      <View style={styles.form}>
+        <Text>Register</Text>
+        <TextInput
+          style={styles.input}
+          placeholder={strings.PLACEHOLDER_USERNAME}
+          value={credentials.username}
+          autoCorrect={false}
+          textContentType="familyName"
+          onChangeText={value => onChangeTextInputHandler('username', value)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder={strings.PLACEHOLDER_EMAIL}
+          value={credentials.email}
+          textContentType="emailAddress"
+          onChangeText={value => onChangeTextInputHandler('email', value)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder={strings.PLACEHOLDER_PASSWORD}
+          secureTextEntry
+          value={credentials.password}
+          onChangeText={value => onChangeTextInputHandler('password', value)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder={strings.PLACEHOLDER_REPEAT_PASSWORD}
+          secureTextEntry
+          value={credentials.repeat_password}
+          onChangeText={value => onChangeTextInputHandler('repeat_password', value)}
+        />
+        <View style={styles.buttonsSection}>
+          <Button title="Cancel" onPress={goBack} />
+          <Button primary title="Sign Up" onPress={() => {}} />
+        </View>
+      </View>
+    </KeyboardAvoidingView>
   );
+};
+
+Register.propTypes = {
+  navigation: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default Register;
