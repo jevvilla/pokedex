e6e9e7fb02003ef9204ffd15f7dad51ca94bb06c
jevvilla <juan.es-19@hotmail.com>
2019-12-14 07:02:54 -0500

    register: headerMode was changed in AuthStack to remove header from the Register and Login screens

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -3,7 +3,12 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { Login, Register } from '../screens';
 import * as routes from './routes';
 
-export default createStackNavigator({
-  [routes.LOGIN]: { screen: Login },
-  [routes.REGISTER]: { screen: Register },
-});
+export default createStackNavigator(
+  {
+    [routes.LOGIN]: { screen: Login },
+    [routes.REGISTER]: { screen: Register },
+  },
+  {
+    headerMode: 'none',
+  },
+);
