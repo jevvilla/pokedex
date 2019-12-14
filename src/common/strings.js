67838240f71850f5559f3aae1066724f35a64238
jevvilla <juan.es-19@hotmail.com>
2019-12-14 06:55:55 -0500

    register: Register fields Placeholder added to strings file in common folder

---
+++ src/common/strings.js
@@ -0,0 +1,5 @@
+// REGISTER
+export const PLACEHOLDER_USERNAME = 'Username';
+export const PLACEHOLDER_EMAIL = 'Email Address';
+export const PLACEHOLDER_PASSWORD = 'Password';
+export const PLACEHOLDER_REPEAT_PASSWORD = 'Repeat Password';
