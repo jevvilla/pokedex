906da04c823c4797c5320b8b2a2ddd49f3923168
jevvilla <juan.es-19@hotmail.com>
2019-12-15 06:45:25 -0500

    login: user tools file was updated, login, logout and getLoggedUser were implemented

--- src/common/tools/user.js
+++ src/common/tools/user.js
@@ -19,3 +19,31 @@ export const getUser = user => {
     });
   });
 };
+
+export const login = async user => {
+  try {
+    const result = await getUser(user);
+    await AsyncStorage.setItem('@loggedUser', result);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+export const logout = async () => {
+  try {
+    const result = AsyncStorage.removeItem('@loggedUser');
+    return result;
+  } catch (err) {
+    return false;
+  }
+};
+
+export const getLoggedUser = async () => {
+  try {
+    const result = AsyncStorage.getItem('@loggedUser');
+    return result;
+  } catch (err) {
+    return false;
+  }
+};
