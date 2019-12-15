0d69d4f2918fad319e9f642aef413c01e082c562
jevvilla <juan.es-19@hotmail.com>
2019-12-15 09:50:13 -0500

    home: headerLayoutPreset added to appStack navigation

--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -2,13 +2,21 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import { Home } from '../screens';
-
+import { defaultHeader } from '../common/styles';
 import * as routes from './routes';
 
-const Navigator = createStackNavigator({
-  [routes.HOME]: {
-    screen: Home,
+const Navigator = createStackNavigator(
+  {
+    [routes.HOME]: {
+      screen: Home,
+    },
   },
-});
+  {
+    headerLayoutPreset: 'center',
+    defaultNavigationOptions: {
+      headerStyle: defaultHeader,
+    },
+  },
+);
 
 export default createAppContainer(Navigator);
