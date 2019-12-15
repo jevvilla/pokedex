edff7e3e380ff403fa69bb1fd61760c557cbae16
jevvilla <juan.es-19@hotmail.com>
2019-12-15 05:42:22 -0500

    login: strings for Login screen added

--- src/common/strings.js
+++ src/common/strings.js
@@ -3,3 +3,9 @@ export const PLACEHOLDER_USERNAME = 'Username';
 export const PLACEHOLDER_EMAIL = 'Email Address';
 export const PLACEHOLDER_PASSWORD = 'Password';
 export const PLACEHOLDER_REPEAT_PASSWORD = 'Repeat Password';
+
+// LOGIN
+export const LOGIN_BUTTON = 'Login';
+export const CREATE_ACCOUNT = 'Create account';
+export const LOGIN_ERROR = 'Error, try later';
+export const NOT_EXISTING_USER = 'Error, user not found';
