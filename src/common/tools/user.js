172e66c8e30c5b375802dab10a3b41fcb74baad2
jevvilla <juan.es-19@hotmail.com>
2019-12-15 05:43:37 -0500

    login: user tools updated, getUser function added and register updated

--- src/common/tools/user.js
+++ src/common/tools/user.js
@@ -3,7 +3,19 @@ import AsyncStorage from '@react-native-community/async-storage';
 export const register = async user => {
   try {
     await AsyncStorage.setItem(`@${user.password}${user.email}`, JSON.stringify(user));
+    return true;
   } catch (err) {
-    console.log(err);
+    return false;
   }
+};
+
+export const getUser = user => {
+  return new Promise((resolve, reject) => {
+    AsyncStorage.getItem(`@${user.password}${user.email}`, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(result);
+    });
+  });
 };
