432a4d74fbad8667870ae7e164d95a6406623165
jevvilla <juan.es-19@hotmail.com>
2019-12-15 05:45:08 -0500

    login: UI wrapped up for loggin screen

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
+import validateJs from 'validate.js';
 
+import validations from './validation';
+import { getUser } from '../../common/tools/user';
+import { Button } from '../../common/components';
 import * as routes from '../../navigation/routes';
+import * as strings from '../../common/strings';
 
 import styles from './styles';
 
 const Login = props => {
+  const [credentials, setCredentials] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const onChangeTextInputHandler = (key, value) => setCredentials({ ...credentials, [key]: value });
+
+  const onLogin = async () => {
+    if (isFormValid(credentials)) {
+      try {
+        const user = await getUser(credentials);
+        if (user) {
+          return goToHome();
+        }
+        return setErrors({ login: strings.NOT_EXISTING_USER });
+      } catch (err) {
+        setErrors({ login: strings.LOGIN_ERROR });
+      }
+    }
+  };
+
+  const isFormValid = fields => {
+    const formErrors = validateJs(fields, validations);
+    if (formErrors) {
+      setErrors(formErrors);
+      return false;
+    }
+    setErrors({});
+    return true;
+  };
+
   const goToRegister = () => {
     const { navigation } = props;
 
@@ -22,13 +56,35 @@ const Login = props => {
 
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
-      <TouchableOpacity onPress={goToRegister}>
-        <Text>go to register</Text>
-      </TouchableOpacity>
-      {/* <TouchableOpacity onPress={goToHome}>
-        <Text>go to Home</Text>
-      </TouchableOpacity> */}
+      <View style={styles.form}>
+        <Text>Login</Text>
+        <View style={styles.row}>
+          <TextInput
+            style={styles.input}
+            placeholder={strings.PLACEHOLDER_EMAIL}
+            value={credentials.email}
+            autoCapitalize="none"
+            textContentType="emailAddress"
+            onChangeText={value => onChangeTextInputHandler('email', value)}
+          />
+          <Text style={styles.errorLabel}>{errors?.email}</Text>
+        </View>
+        <View style={styles.row}>
+          <TextInput
+            style={styles.input}
+            placeholder={strings.PLACEHOLDER_PASSWORD}
+            secureTextEntry
+            value={credentials.password}
+            onChangeText={value => onChangeTextInputHandler('password', value)}
+          />
+          <Text style={styles.errorLabel}>{errors?.password}</Text>
+        </View>
+        <Button primary title={strings.LOGIN_BUTTON} onPress={onLogin} />
+        <Text style={styles.errorLabel}>{errors?.login}</Text>
+        <TouchableOpacity onPress={goToRegister}>
+          <Text style={styles.registrationLabel}>{strings.CREATE_ACCOUNT}</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -1,7 +1,37 @@
 import { StyleSheet } from 'react-native';
 
+import { colors, sizes } from '../../common/styles';
+
 export default StyleSheet.create({
   container: {
-    marginTop: 100,
+    flex: 1,
+    backgroundColor: colors.background,
+    paddingHorizontal: sizes.base * 2,
+  },
+  form: {
+    marginTop: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    width: '100%',
+    height: 45,
+    paddingHorizontal: sizes.base,
+    borderRadius: sizes.radius,
+    borderWidth: StyleSheet.hairlineWidth,
+  },
+  errorLabel: {
+    color: colors.red,
+    fontSize: 12,
+  },
+  row: {
+    width: '100%',
+    marginVertical: 8,
+  },
+  registrationLabel: {
+    color: colors.black,
+    fontWeight: 'bold',
+    fontSize: 16,
+    paddingVertical: sizes.base,
   },
 });
