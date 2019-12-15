2ef668745dfb5fbc34ac0a42d68a909e2f77305c
jevvilla <juan.es-19@hotmail.com>
2019-12-15 06:48:36 -0500

    login: AuthLoadingUserScreen added to the navigation main stack

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,16 +1,18 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
+import { AuthLoadingScreen } from '../screens';
 import AppStack from './AppStack';
 import AuthStack from './AuthStack';
 import * as routes from './routes';
 
 const switchNavigator = createSwitchNavigator(
   {
+    [routes.SWITCH_AUTHLOADING]: AuthLoadingScreen,
     [routes.SWITCH_AUTH]: AuthStack,
     [routes.SWITCH_APP]: AppStack,
   },
   {
-    initialRouteName: routes.SWITCH_LOGIN,
+    initialRouteName: routes.SWITCH_AUTHLOADING,
   },
 );
 
--- src/navigation/routes.js
+++ src/navigation/routes.js
@@ -1,7 +1,7 @@
 // Main Switch Navigator
 export const SWITCH_APP = 'SWITCH_APP';
 export const SWITCH_AUTH = 'SWITCH_AUTH';
-
+export const SWITCH_AUTHLOADING = 'SWITCH_AUTHLOADING';
 // Login
 export const LOGIN = 'LOGIN';
 export const REGISTER = 'REGISTER';
