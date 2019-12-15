846597e0f5f58fd7d6856380499f6277b8df8cc9
jevvilla <juan.es-19@hotmail.com>
2019-12-15 06:48:13 -0500

    login: AuthLoadingUserScreen created to manage the switching between stacks in the app

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,6 @@
+import AuthLoadingScreen from './AuthLoadingScreen';
 import Home from './Home';
 import Login from './Login';
 import Register from './Register';
 
-export { Home, Login, Register };
+export { AuthLoadingScreen, Home, Login, Register };
